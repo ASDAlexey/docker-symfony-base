59ac92a9e078318f3d7710fbfec402fa14dbb572
Alexey Popov <alexey.popov@umbrella-web.com>
2018-01-07 14:37:22 +0300

    update symfony parameters

--- symfony_config.sh
+++ symfony_config.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-{ \
-    echo 'parameters:'; \
-    echo '  database_host: db'; \
-    echo '  database_port: 3306'; \
-    echo '  database_name: '${MYSQL_DATABASE}''; \
-    echo '  database_user: '${MYSQL_USER}''; \
-    echo '  database_password: '${MYSQL_PASSWORD}''; \
-}| tee ./app/config/parameters.yml
+
+cat <<EOF > app/config/parameters.yml
+parameters:
+    database_host: db
+    database_port: 3306
+    database_name: $MYSQL_DATABASE
+    database_user: $MYSQL_USER
+    database_password: $MYSQL_PASSWORD
+    secret: $SYMFONY_SECRET
+    product_image:
+        uri_prefix: /images/products/
+        upload_destination: '%kernel.root_dir%/../web/images/products'
+EOF
